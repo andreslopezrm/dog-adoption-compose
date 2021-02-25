e4b84ebcc822a192a6dbf99511c8e99136df2217
Andres <andres_lopez_rm@hotmail.com>
2021-02-25 09:16:03 -0600

    [UPDATE] fixeds by spotlessApply

--- app/src/main/java/com/example/androiddevchallenge/ui/composables/DogList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/composables/DogList.kt
@@ -1,16 +1,31 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.composables
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.androiddevchallenge.repositories.DogRepository
 
 @Composable
@@ -30,7 +45,8 @@ fun DogList(navController: NavController) {
                 contentColor = Color.White,
                 elevation = 12.dp
             )
-        }) {
+        }
+    ) {
 
         LazyColumn(
             contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
@@ -39,7 +55,5 @@ fun DogList(navController: NavController) {
                 DogItem(dog = dog, navController = navController)
             }
         }
-
     }
-
 }
