c08dfbb0344d36403c908d41400afe3c33f91708
Andres <andres_lopez_rm@hotmail.com>
2021-02-24 18:46:44 -0600

    [ADD] initial commit

---
+++ app/src/main/java/com/kal/dogadoptioncompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.kal.dogadoptioncompose
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kal.dogadoptioncompose.ui.theme.DogAdoptionComposeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DogAdoptionComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DogAdoptionComposeTheme {
+        Greeting("Android")
+    }
+}
